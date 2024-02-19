ee3202fa553dc55ff6f34312aac8fc6fdc04d20e
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-02-18 20:07:26 -0600

    Agregar archivos de proyecto.

---
+++ Exercise 1 U2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Exercise_1_U2
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            formula velo = new formula();
+            velo.v_carro(1000,600,1.6667);
+
+            Console.WriteLine("~The speed of a car~");
+            Console.WriteLine("Distance(m)= " + velo.Distance);
+            Console.WriteLine("time(s)= " + velo.Time);
+            Console.WriteLine("velocity=" + velo.Velocity + "m/s");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Exercise 1 U2/formula.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1_U2
+{
+    public class formula
+    {
+        private int distance;
+        private int time;
+        private double velocity;
+
+        public int Distance { get => distance; set => distance = value; }
+        public int Time { get => time; set => time = value; }
+        public double Velocity { get => velocity; set => velocity = value; }
+
+        public void v_carro(int _distance, int _time, double _velocity)
+        {
+            this.distance = _distance;
+            this.time= _time;
+            this.velocity = _velocity;
+        }
+
+
+    }
+}
